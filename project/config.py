22a2faab8a951f63b9dc15d5ffd75437c55337a5
Antiguru2 <antiguru@yansex.ru>
2021-09-24 12:01:32 +0500

    Код ревью

--- project/config.py
+++ project/config.py
@@ -1,4 +1,7 @@
-token = 
+token = ''
+url_1 = f"https://екатеринбург.рф/%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D1%8F%D0%BC/%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%B2%D0%B7%D0%B0%D0%B8%D0%BC%D0%BE%D0%B4%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%B8%D0%B5%D0%9E%D0%9E/%D0%A2%D0%A4"
+url_2 = f"https://екатеринбург.рф/%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D1%8F%D0%BC/%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%B2%D0%B7%D0%B0%D0%B8%D0%BC%D0%BE%D0%B4%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%B8%D0%B5%D0%9E%D0%9E/%D1%81%D1%80%D0%BE%D1%87%D0%BD%D0%BE"
+name_file_1 = 'last_update_date1.txt'
+name_file_2 = 'last_update_date2.txt'
 chanell_from = '@SnowNews_Coolside'
 channel_name = '-1001205451700'
-last_date_change = ''
--- project/parsing_script.py
+++ project/parsing_script.py
@@ -9,41 +9,27 @@ import config
 
     
 
-def parsing_date():
-    response1 = requests.get(f"https://екатеринбург.рф/%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D1%8F%D0%BC/%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%B2%D0%B7%D0%B0%D0%B8%D0%BC%D0%BE%D0%B4%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%B8%D0%B5%D0%9E%D0%9E/%D0%A2%D0%A4", verify = False)
-    response2 = requests.get(f"https://екатеринбург.рф/%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D1%8F%D0%BC/%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%B2%D0%B7%D0%B0%D0%B8%D0%BC%D0%BE%D0%B4%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%B8%D0%B5%D0%9E%D0%9E/%D1%81%D1%80%D0%BE%D1%87%D0%BD%D0%BE", verify = False)
-    if response1.status_code == 200 and response2.status_code == 200:
-        srs1 = response1.text
-        srs2 = response2.text
-        soup1 = BeautifulSoup(srs1, "html.parser")
-        soup2 = BeautifulSoup(srs2, "html.parser")
-        fout1 =  codecs.open('project/last_update_date1.txt', 'r', "utf-8")
-        fout2 =  codecs.open('project/last_update_date2.txt', 'r', "utf-8")
+def parsing_date(url, file):
+    response = requests.get(url, verify = False)
+    if response.status_code == 200:
+        srs = response.text
+        soup = BeautifulSoup(srs, "html.parser")
+        fout =  codecs.open(f'project/{file}', 'r', "utf-8")
 
-        if fout1.read() != str(soup1.find(class_='last-update-date').string) or fout2.read() != str(soup2.find(class_='last-update-date').string) :
-            fout1 =  codecs.open('project/last_update_date1.txt', 'r', "utf-8")
-            fout2 =  codecs.open('project/last_update_date2.txt', 'r', "utf-8")
-            print('Не совпадают')
-            if fout1.read() != str(soup1.find(class_='last-update-date').string):  
-                fout1 =  codecs.open('project/last_update_date1.txt', 'w+', "utf-8")                 
-                fout1.seek(0)
-                update_date_as_syte1 = soup1.find(class_='last-update-date').string
-                send_in_telegram(update_date_as_syte1)
-                fout1.write(update_date_as_syte1)
-                fout1.close()
-            elif fout2.read() != str(soup2.find(class_='last-update-date').string):
-                fout2 =  codecs.open('project/last_update_date2.txt', 'w+', "utf-8")
-                fout2.seek(0)
-                update_date_as_syte2 = soup2.find(class_='last-update-date').string
-                send_in_telegram(update_date_as_syte2)
-                fout2.write(update_date_as_syte2)
-                fout2.close()
+        if fout.read() != str(soup.find(class_='last-update-date').string):
+            fout =  codecs.open('project/last_update_date1.txt', 'w+', "utf-8")
+            print('Не совпадают')              
+            fout.seek(0)
+            update_date_as_syte = soup.find(class_='last-update-date').string
+            send_in_telegram(update_date_as_syte)
+            fout.write(update_date_as_syte)
+            fout.close()
 
         else:
-            fout1.close()
-            print('Одинаковые')
+            fout.close()
+            print('Cовпадают')
     else:
-        print(f'1={response1.status_code},2={response2.status_code}')
+        print(response.status_code)
 
 
 
@@ -56,5 +42,7 @@ def send_in_telegram(mesege_for_telegram):
     print(response)
 
 
-
-parsing_date()
+print('Первый url')
+parsing_date(config.url_1, config.name_file_1)
+print('Второй url')
+parsing_date(config.url_2, config.name_file_2)
